99a5aeae47e4edb04d1b0a48e582d9bbe64a3bc9
youngbin.lee <youngbin.lee@gmail.com>
2016-06-27 16:16:12 +0900

    First Commit

---
+++ app/src/main/java/com/multicampus/androidlab/ch04/ImageViewActivity.java
@@ -0,0 +1,13 @@
+package com.multicampus.androidlab.ch04;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ch04_activity_image_view);
+    }
+}
---
+++ app/src/main/java/com/multicampus/androidlab/ch04/TextViewActivity.java
@@ -0,0 +1,15 @@
+package com.multicampus.androidlab.ch04;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.multicampus.androidlab.R;
+
+public class TextViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ch04_activity_text_view);
+    }
+}
