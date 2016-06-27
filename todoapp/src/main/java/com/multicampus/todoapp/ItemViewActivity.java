5262df0c866877954ceb6beb2a59836a5bf442fe
youngbin.lee <youngbin.lee@gmail.com>
2016-06-27 17:27:19 +0900

    File update for TodoApp

--- todoapp/src/main/java/com/multicampus/todoapp/ItemViewActivity.java
+++ todoapp/src/main/java/com/multicampus/todoapp/ItemViewActivity.java
@@ -9,5 +9,6 @@ public class ItemViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_view);
+
     }
 }
